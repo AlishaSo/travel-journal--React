8d57440a5f55b9444b2449a8559635443a1134a0
Alisha Soriano <sorianoalisha@gmail.com>
2022-03-02 17:21:47 -0500

    Finish adding destination objects and add proper styles

--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 export default [
   {
     id: 1,
-    title: 'Punta Cana',
-    location: 'Dominican Republic',
-    googleMapsUrl: 'https://www.google.com/maps/place/Punta+Cana,+Dominican+Republic/@18.6406654,-68.6117751,11z/data=!3m1!4b1!4m5!3m4!1s0x8ea891645dcbfe77:0x61881cfaed12f6f3!8m2!3d18.5600761!4d-68.372535',
-    startDate: '24 May 2023',
-    endDate: '8 June 2023',
-    description: "Punta Cana, the easternmost tip of the Dominican Republic, abuts the Caribbean Sea and the Atlantic Ocean. It's a region known for its 32km stretch of beaches and clear waters. The Bávaro area and Punta Cana combine to form what's known as La Costa del Coco, or the Coconut Coast, an area of lavish, all-inclusive resorts. It's popular for zip-lining, windsurfing, kayaking and sailing.",
-    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/7/70/Dominicana-Punta_Cana.jpg"
+    title: 'Addis Ababa',
+    location: 'Ethiopia',
+    googleMapsUrl: 'https://www.google.com/maps/place/Addis+Ababa,+Ethiopia/@8.9631505,38.6380581,11z/data=!3m1!4b1!4m5!3m4!1s0x164b85cef5ab402d:0x8467b6b037a24d49!8m2!3d8.9806034!4d38.7577605',
+    startDate: '24 May, 2023',
+    endDate: '8 June, 2023',
+    description: 'Addis Ababa, Ethiopia’s sprawling capital in the highlands bordering the Great Rift Valley, is the country’s commercial and cultural hub. Its National Museum exhibits Ethiopian art, traditional crafts and prehistoric fossils, including replicas of the famous early hominid, "Lucy." The burial place of the 20th-century emperor Haile Selassie, copper-domed Holy Trinity Cathedral, is a neo-baroque architectural landmark.',
+    imgUrl: "https://images.adsttc.com/media/images/59b6/bc7c/b22e/38e2/0300/00ba/slideshow/PORTADA_lideta_mercato-vilalta-01.jpg?1505148023"
   },
 
   {
@@ -15,20 +15,42 @@ export default [
     title: 'Park and National Palace of Pena',
     location: 'Portugal',
     googleMapsUrl: 'https://www.google.com/maps/place/Park+and+National+Palace+of+Pena/@38.7875851,-9.3906089,15z/data=!4m2!3m1!1s0x0:0x77271a7dcb673b58?sa=X&ved=2ahUKEwiBkfb5oqj2AhXXkokEHV_qAcEQ_BJ6BAg5EAU',
-    startDate: '5 November 2023',
-    endDate: '12 November 2023',
+    startDate: '5 November, 2023',
+    endDate: '12 November, 2023',
     description: "The Pena Palace is a Romanticist castle in São Pedro de Penaferrim, in the municipality of Sintra, on the Portuguese Riviera. The castle stands on the top of a hill in the Sintra Mountains above the town of Sintra, and on a clear day it can be easily seen from Lisbon and much of its metropolitan area.",
-    imgUrl: "https://images.unsplash.com/photo-1562195168-c82fea0f0953?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+    imgUrl: "https://images.unsplash.com/photo-1498730515581-993ec29aaab3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2068&q=80"
   },
 
   {
     id: 3,
+    title: 'Punta Cana',
+    location: 'Dominican Republic',
+    startDate: '1 January, 2024',
+    endDate: '1 February, 2024',
+    googleMapsUrl: 'https://www.google.com/maps/place/Punta+Cana,+Dominican+Republic/@18.6406654,-68.6117751,11z/data=!3m1!4b1!4m5!3m4!1s0x8ea891645dcbfe77:0x61881cfaed12f6f3!8m2!3d18.5600761!4d-68.372535',
+    description: "Punta Cana, the easternmost tip of the Dominican Republic, abuts the Caribbean Sea and the Atlantic Ocean. It's a region known for its 32km stretch of beaches and clear waters. The Bávaro area and Punta Cana combine to form what's known as La Costa del Coco, or the Coconut Coast, an area of lavish, all-inclusive resorts. It's popular for zip-lining, windsurfing, kayaking and sailing.",
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/7/70/Dominicana-Punta_Cana.jpg"
+  },
+
+  {
+    id: 4,
+    title: 'Jigawa',
+    location: 'Nigeria',
+    googleMapsUrl: 'https://www.google.com/maps?client=firefox-b-1-d&q=Jigawa&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiLoNqzuaj2AhVcjIkEHdk2CbwQ_AUoAnoECAIQBA',
+    startDate: '18 April, 2024',
+    endDate: '30 April, 2024',
+    description: "Jigawa State is one of the 36 states of Nigeria, located in the northern region of the country. Created in 1991 from the northeastern-most region of Kano State, Jigawa State is located on the border with Nigeria's national border with the Republic of the Niger. The state capital and largest city is Dutse.",
+    imgUrl: "https://i.pinimg.com/736x/2e/cd/5d/2ecd5daafaa6fc589dbf13ef8a41887b.jpg"
+  },
+
+  {
+    id: 5,
     title: 'Louvre Museum',
     location: 'France',
+    startDate: '3 August, 2024',
+    endDate: '15 August, 2024',
     googleMapsUrl: 'https://www.google.com/maps/place/Louvre+Museum/@48.8606111,2.337644,15z/data=!4m2!3m1!1s0x0:0xb975fcfa192f84d4?sa=X&ved=2ahUKEwi53YjFpKj2AhXAlIkEHQTWDwgQ_BJ6BAhSEAU',
-    startDate: '1 January 2024',
-    endDate: '1 February 2024',
     description: "The Louvre, or the Louvre Museum, is the world's most-visited museum, and a historic landmark in Paris, France. It is the home of some of the best-known works of art, including the Mona Lisa and the Venus de Milo.",
     imgUrl: "https://images.unsplash.com/photo-1560425946-7d5830202765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
-  }
+  },
 ]
