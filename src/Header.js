fe1f4697a872e6cd98357a2d49e33fae799961d6
Alisha Soriano <sorianoalisha@gmail.com>
2022-03-02 12:54:50 -0500

    Create Header component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import globeImg from './images/globe.png';
+
+export default function Header() {
+  return (
+    <header>
+      <img src={globeImg} alt=''/>
+      <p>thoaljfdaldjfl</p>
+    </header>
+  )
+}
